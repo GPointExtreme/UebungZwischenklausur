caf9b058a50e8ccd70374641ad703fd8cc470d42
GPointExtreme <dominik.geiger@hotmail.com>
2015-12-05 18:15:45 +0100

    Nochmal

--- src/FourWinsDemo.java
+++ src/FourWinsDemo.java
@@ -5,11 +5,17 @@ public class FourWinsDemo {
 		// TODO Auto-generated method stub
 		FourWins Spiel = new FourWins(5, 5);
 		Spiel.playerA(0, 0);
+		System.out.println(Spiel.winner());
 		Spiel.playerB(1, 0);
+		System.out.println(Spiel.winner());
 		Spiel.playerA(0, 1);
+		System.out.println(Spiel.winner());
 		Spiel.playerB(1, 1);
+		System.out.println(Spiel.winner());
 		Spiel.playerA(0, 2);
+		System.out.println(Spiel.winner());
 		Spiel.playerB(1, 2);
+		System.out.println(Spiel.winner());
 		Spiel.playerA(0, 3);
 		System.out.println(Spiel.winner());
 		
